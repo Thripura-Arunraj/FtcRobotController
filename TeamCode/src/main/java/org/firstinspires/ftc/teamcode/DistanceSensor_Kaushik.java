da372271ca4ba0d61ccbde99e2911c2a56992138
TheAwesomeKid02 <69116138+TheAwesomeKid02@users.noreply.github.com>
2022-11-13 10:42:02 -0500

    Simple Distance Sensor code
    can still be changed and improved

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DistanceSensor_Kaushik.java
@@ -0,0 +1,36 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DistanceSensor;
+
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+
+
+@Autonomous
+public class DistanceSensor_Kaushik extends LinearOpMode {
+    private DcMotor LeftForward, LeftBack, RightForward, RightBack;
+    private DistanceSensor distanceSensor;
+
+    @Override
+    public void runOpMode() {
+        String name_of_distance_sensor;
+
+        LeftBack = hardwareMap.dcMotor.get("LeftBack");
+        LeftForward = hardwareMap.dcMotor.get("LeftForward");
+        RightForward = hardwareMap.dcMotor.get("RightForward");
+        RightBack = hardwareMap.dcMotor.get("RightBack");
+
+        distanceSensor = hardwareMap.get(DistanceSensor.class, name_of_distance_sensor);
+
+        waitForStart();
+
+        while(opModeIsActive()) {
+            telemetry.addData("Distance (cm)", distanceSensor.getDistance(DistanceUnit.CM));
+
+            telemetry.update();
+        }
+    }
+
+}
